0b29b431969689aa3eb2fbab252789dcd5d205fd
Mouhmmad Moujarkash <mouhmmad488@gmail.com>
2020-10-05 19:20:55 +0300

    - added launcher icon

--- lib/ui/src/app.dart
+++ lib/ui/src/app.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:windy/ui/screens/home_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: HomeScreen(),
     );
   }
 }
